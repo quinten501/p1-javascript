5bcf5b5ebbe3faa903c2283cefe7b9c639009721
Quinten van Oostrom <Quintenvanoostrom@gmail.com>
2022-09-15 13:54:17 +0200

    opdracht3.1

---
+++ data-types/app.js
@@ -0,0 +1,26 @@
+let myName = "Quinten";
+document.write(myName);
+let myAge = 20;
+document.write(myAge);
+let diploma = true;
+document.write(diploma);
+let passed;
+document.write(passed);
+
+let num1 = 7;
+let num2 = 5;
+
+let answer = num1*num2;
+document.write(answer);
+
+let answer2 = num1/num2;
+document.write(answer2);
+
+let answer3 = num1+num2;
+document.write(answer3);
+
+let answer4 = num1-num2;
+document.write(answer4);
+
+let answer5 = num1%num2;
+document.write(answer5);
