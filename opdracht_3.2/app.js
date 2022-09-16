410199546d7c7642979ed976ec717f5bfe828ead
Quinten van Oostrom <Quintenvanoostrom@gmail.com>
2022-09-16 09:34:56 +0200

    Opdracht 3.2

---
+++ opdracht_3.2/app.js
@@ -0,0 +1,17 @@
+let num = 5;
+let num2 = 5;
+
+if(num < 5){
+    console.log("ik ben kleiner dan 5");
+} else if(num > 5){
+    console.log("ik ben hoger dan 5");
+} else if(num = 5){
+    console.log("ik ben 5");
+}
+
+if(num < 6 && num2 == 5){
+    console.log("num is lager dan 6 en num2 is gelijk aan 5");
+}
+if(num < 5 || num2 == 5){
+    console.log("num is lager dan 5 of num 2 is gelijk aan 5");
+}
