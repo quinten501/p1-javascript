283afdd1ce37cffc674db7d55f3b75294801a472
Quinten van Oostrom <Quintenvanoostrom@gmail.com>
2022-09-22 13:51:01 +0200

    oefenen 2

---
+++ oefenen2/app.js
@@ -0,0 +1,8 @@
+const kleur_aanpassen = function() {
+let title = "title";
+let element = document.getElementById(title);
+
+element.style.color = "red";
+element.style.fontWeight = "normal";
+element.innerText = "Ik ben de beste";
+}
