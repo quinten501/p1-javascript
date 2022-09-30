51e03b6104394402f099e6f8b38826d924a182e7
Quinten van Oostrom <Quintenvanoostrom@gmail.com>
2022-09-30 10:22:02 +0200

    opdracht 7.1

---
+++ opdracht_7.1/app.js
@@ -0,0 +1,12 @@
+let txt = document.getElementById("txt");
+
+let budget = 100;
+let product = 60;
+
+if (budget >= product) {
+    txt.innerText = "U heeft genoeg geld!";
+    txt.style.color = "green";
+} else {
+    txt.innerText = "Helaas, te weinig geldt"
+    txt.style.color = "red";
+}
