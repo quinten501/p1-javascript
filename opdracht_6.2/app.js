18edf1b59eed2f91bb4f5373e28bde8e31c51242
Quinten van Oostrom <Quintenvanoostrom@gmail.com>
2022-09-30 10:07:00 +0200

    opdracht 6.2

---
+++ opdracht_6.2/app.js
@@ -0,0 +1,7 @@
+const kleur_aanpassen = function() {
+    let title = "title";
+    let element = document.getElementById(title);
+    
+    element.style.color = "red";
+    element.innerText = "Inhoud aangepast!";
+    }
