b399647f0ef1026bb1f1402dec12cb1b332fdb9d
Quinten van Oostrom <Quintenvanoostrom@gmail.com>
2022-10-13 14:11:45 +0200

    Opdracht 8.2

--- opdracht_8.1/app.js
+++ opdracht_8.1/app.js
@@ -3,21 +3,33 @@ let num2 = document.getElementById("num2");
 let txt = document.getElementById("text");
 
 const plus = function() {
+    if ((num.value <= 0) || (num2.value <= 0)) {
+    txt.innerText = "Het getal is te laag"
+} else {
     answer = Number(num.value) + Number(num2.value);
     txt.innerText = "Het antwoord is: " + answer;
-}
+}}
 
 const keer = function() {
+    if ((num.value <= 0) || (num2.value <= 0)) {
+    txt.innerText = "Het getal is te laag"
+} else {
     answer = num.value * num2.value;
     txt.innerText = "Het antwoord is: " + answer;
-}
+}}
 
 const min = function() {
+    if ((num.value <= 0) || (num2.value <= 0)) {
+    txt.innerText = "Het getal is te laag"
+} else {
     answer = num.value - num2.value;
     txt.innerText = "Het antwoord is: " + answer;
-}
+}}
 
 const gedeeddoor = function() {
+    if ((num.value <= 0) || (num2.value <= 0)) {
+    txt.innerText = "Het getal is te laag"
+} else {
     answer = num.value / num2.value;
     txt.innerText = "Het antwoord is: " + answer;
-}
+}}
